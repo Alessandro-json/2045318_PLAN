10e328e6f80ec632e950d6a06ccde202190f2318
Luca Natilla <dev.dead.tech@gmail.com>
2026-03-08 18:48:35 +0100

    fix rabbitmq race condition on disconnect

--- source/backend/backend.py
+++ source/backend/backend.py
@@ -4,6 +4,7 @@ import asyncio
 from models.models import NormalizedData
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from aio_pika import connect_robust, ExchangeType
+from websockets.exceptions import ConnectionClosedOK
 
 
 RABBIT_HOST = os.getenv('RABBIT_HOST', 'localhost')
@@ -24,8 +25,11 @@ class ConnectionManager:
         self.connections.remove(websocket)
 
     async def broadcast(self, message):
-        for connection in self.connections:
-            await connection.send_text(message)
+        for connection in self.connections[:]:
+            try:
+                await connection.send_text(message)
+            except (WebSocketDisconnect, ConnectionClosedOK, RuntimeError):
+                self.connections.remove(connection)
 
 
 manager = ConnectionManager()
@@ -49,15 +53,18 @@ async def websocket_endpoint(websocket: WebSocket):
 async def _rabbitmq_consumer():
     connection = await connect_robust(f'amqp://guest:guest@{RABBIT_HOST}/')
 
-    async with connection:
-        channel = await connection.channel()
-        await channel.declare_exchange(EXCHANGE_NAME, ExchangeType.TOPIC)
+    try:
+        async with connection:
+            channel = await connection.channel()
+            await channel.declare_exchange(EXCHANGE_NAME, ExchangeType.TOPIC)
 
-        queue = await channel.declare_queue(exclusive=True)
-        await queue.bind(exchange=EXCHANGE_NAME, routing_key='data.#')
+            queue = await channel.declare_queue(exclusive=True)
+            await queue.bind(exchange=EXCHANGE_NAME, routing_key='data.#')
 
-        async with queue.iterator() as queue_iter:
-            async for message in queue_iter:
-                async with message.process():
-                    payload = message.body.decode()
-                    await manager.broadcast(payload)
+            async with queue.iterator() as queue_iter:
+                async for message in queue_iter:
+                    async with message.process():
+                        payload = message.body.decode()
+                        await manager.broadcast(payload)
+    except Exception as e:
+        print(f'error: rabbitmq consumer encountered an error: {e}')
